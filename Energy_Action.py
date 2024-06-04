b77bacb7a423551c8377e8b4743f460a81a6d8c0
Firsov Viktor <firsov.va@phystech.edu>
2024-06-04 11:48:08 +0300

    Energy_Action.py updated

--- Energy_Action.py
+++ Energy_Action.py
@@ -15,24 +15,21 @@ with open(filename) as file:
 
         line = line.rstrip()
         task_id    = line.split(":")[1].split(",")[0]
-        Action     = line.split(":")[2].split(",")[0]
-        Energy     = line.split(":")[3].split(",")[0]
-        Energy_std = line.split(":")[4].split(",")[0]
+        Action_re  = line.split(":")[2].split(",")[0]
+        Action_im  = line.split(":")[3].split(",")[0]
+        Energy_re  = line.split(":")[4].split(",")[0]
+        Energy_im  = line.split(":")[5].split(",")[0]
+        Energy_std = line.split(":")[6].split(",")[0]
 
         new_dict["Nr"]         =   int(task_id.split("_")[1])
         new_dict["Nt"]         =   int(task_id.split("_")[3])
         new_dict["alpha"]      = float(task_id.split("_")[5])
         new_dict["Dimensions"] = float(task_id.split("_")[7])
 
-        if ("+" in Action): new_dict["Action_re"] = float(Action.split("+")[0])
-        if ("-" in Action): new_dict["Action_re"] = float(Action.split("-")[0])
-        if ("+" in Action): new_dict["Action_im"] = float(Action.split("+")[1][:-1])
-        if ("-" in Action): new_dict["Action_im"] = float(Action.split("-")[1][:-1])
-        
-        if ("+" in Energy): new_dict["Energy_re"] = float(Energy.split("+")[0])
-        if ("-" in Energy): new_dict["Energy_re"] = float(Energy.split("-")[0])
-        if ("+" in Energy): new_dict["Energy_im"] = float(Energy.split("+")[1][:-1])
-        if ("-" in Energy): new_dict["Energy_im"] = float(Energy.split("-")[1][:-1])
+        new_dict["Action_re"]  = float(Action_re.split(",")[0])
+        new_dict["Action_im"]  = float(Action_im.split(",")[0])
+        new_dict["Energy_re"]  = float(Energy_re.split(",")[0])
+        new_dict["Energy_im"]  = float(Energy_im.split(",")[0])
 
         new_dict["Energy_std"] = float(Energy_std)
         data.append(new_dict)
